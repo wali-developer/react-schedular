cb0e1aefe951c7475c2e604ff35f394a4dde5ee9
Wali  Ullah <waliullah0540@gmail.com>
2023-02-05 18:29:10 +0500

    updpate

--- pages/admin/agenda/index.js
+++ pages/admin/agenda/index.js
@@ -22,167 +22,167 @@ const index = () => {
     const [tagsOptions, setTagsOptions] = useState([]);
     const [usersList, setUsersList] = useState([]);
 
-    const loadtagDetails = async (t) => {
-        try {
-            const { data } = await API.get(`/api/agenda/tag/${t}/`, header)
-            return data
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    // const loadtagDetails = async (t) => {
+    //     try {
+    //         const { data } = await API.get(`/api/agenda/tag/${t}/`, header)
+    //         return data
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
 
-    const getEventsList = async () => {
-        setLoading(true)
-        try {
-            const { data } = await API.get('/api/agenda/event/', header);
+    // const getEventsList = async () => {
+    //     setLoading(true)
+    //     try {
+    //         const { data } = await API.get('/api/agenda/event/', header);
 
 
-            Promise.all(data?.map(async (event) => {
-                let tagDetails = null
-                if (event?.tag) {
-                    tagDetails = await loadtagDetails(event?.tag)
-                }
-                return {
-                    event_id: event?.id,
-                    title: event?.title,
-                    start: new Date(event?.begin_datetime),
-                    end: new Date(event?.end_datetime),
-                    description: event?.description,
-                    tag: event?.tag,
-                    color: tagDetails ? tagDetails?.color : null
-                }
-            })).then(d => { setEventsList(d) }).catch(e => console.log(e))
+    //         Promise.all(data?.map(async (event) => {
+    //             let tagDetails = null
+    //             if (event?.tag) {
+    //                 tagDetails = await loadtagDetails(event?.tag)
+    //             }
+    //             return {
+    //                 event_id: event?.id,
+    //                 title: event?.title,
+    //                 start: new Date(event?.begin_datetime),
+    //                 end: new Date(event?.end_datetime),
+    //                 description: event?.description,
+    //                 tag: event?.tag,
+    //                 color: tagDetails ? tagDetails?.color : null
+    //             }
+    //         })).then(d => { setEventsList(d) }).catch(e => console.log(e))
 
-            // setEventsList(temp)
-            setLoading(false)
-        } catch (error) {
-            setLoading(false)
-            console.log(error);
-        }
-    }
+    //         // setEventsList(temp)
+    //         setLoading(false)
+    //     } catch (error) {
+    //         setLoading(false)
+    //         console.log(error);
+    //     }
+    // }
 
 
 
     // Edit & Create Event
-    const handleConfirm = async (event, action) => {
-        if (action === "edit") {
-            /** PUT event to remote DB */
-            const payLoad = {
-                begin_datetime: event?.start,
-                end_datetime: event?.end,
-                title: event?.title,
-                description: event?.description,
-                participants: event?.participants,
-                tag: event?.tag
-            }
-            try {
-                const { data } = await API.patch(`/api/agenda/event/${event?.event_id}/`, payLoad, header);
+    // const handleConfirm = async (event, action) => {
+    //     if (action === "edit") {
+    //         /** PUT event to remote DB */
+    //         const payLoad = {
+    //             begin_datetime: event?.start,
+    //             end_datetime: event?.end,
+    //             title: event?.title,
+    //             description: event?.description,
+    //             participants: event?.participants,
+    //             tag: event?.tag
+    //         }
+    //         try {
+    //             const { data } = await API.patch(`/api/agenda/event/${event?.event_id}/`, payLoad, header);
 
-                if (data) {
-                    toast("Event updated successfully")
-                    getEventsList();
-                }
+    //             if (data) {
+    //                 toast("Event updated successfully")
+    //                 getEventsList();
+    //             }
 
 
-            } catch (error) {
-                console.log(error);
-            }
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
 
-        } else if (action === "create") {
-            /**POST event to remote DB */
+    //     } else if (action === "create") {
+    //         /**POST event to remote DB */
 
-            const payLoad = {
-                begin_datetime: event?.start,
-                end_datetime: event?.end,
-                title: event?.title,
-                description: event?.description,
-                participants: event?.participants,
-                tag: event?.tag
-            }
+    //         const payLoad = {
+    //             begin_datetime: event?.start,
+    //             end_datetime: event?.end,
+    //             title: event?.title,
+    //             description: event?.description,
+    //             participants: event?.participants,
+    //             tag: event?.tag
+    //         }
 
-            try {
-                const { data } = await API.post('/api/agenda/event/', payLoad, header);
-                if (data) {
-                    toast("Event schedule successfully")
-                    getEventsList();
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        }
+    //         try {
+    //             const { data } = await API.post('/api/agenda/event/', payLoad, header);
+    //             if (data) {
+    //                 toast("Event schedule successfully")
+    //                 getEventsList();
+    //             }
+    //         } catch (error) {
+    //             console.log(error)
+    //         }
+    //     }
 
-        return new Promise((res, rej) => {
-            setTimeout(() => {
-                res({
-                    ...event,
-                    event_id: event.event_id || Math.random()
-                });
-            }, 500);
-        });
-    };
+    //     return new Promise((res, rej) => {
+    //         setTimeout(() => {
+    //             res({
+    //                 ...event,
+    //                 event_id: event.event_id || Math.random()
+    //             });
+    //         }, 500);
+    //     });
+    // };
 
-    const handleDelete = async (deletedId) => {
-        // Simulate http request: return the deleted id
-        try {
-            const { data } = await API.delete(`/api/agenda/event/${deletedId}`, header);
-            getEventsList();
+    // const handleDelete = async (deletedId) => {
+    //     // Simulate http request: return the deleted id
+    //     try {
+    //         const { data } = await API.delete(`/api/agenda/event/${deletedId}`, header);
+    //         getEventsList();
 
-        } catch (error) {
-            console.log(error);
-        }
-        return new Promise((res, rej) => {
-            setTimeout(() => {
-                res(deletedId);
-            }, 1000);
-        });
-    };
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    //     return new Promise((res, rej) => {
+    //         setTimeout(() => {
+    //             res(deletedId);
+    //         }, 1000);
+    //     });
+    // };
 
-    const getTags = async () => {
-        try {
-            const { data } = await API.get('/api/agenda/tag/', header);
+    // const getTags = async () => {
+    //     try {
+    //         const { data } = await API.get('/api/agenda/tag/', header);
 
-            if (data) {
-                const tempArr = data?.map((item) => {
-                    return {
-                        id: item?.id,
-                        text: item?.name,
-                        value: item?.id,
-                    }
-                })
-                setTagsOptions(tempArr)
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    //         if (data) {
+    //             const tempArr = data?.map((item) => {
+    //                 return {
+    //                     id: item?.id,
+    //                     text: item?.name,
+    //                     value: item?.id,
+    //                 }
+    //             })
+    //             setTagsOptions(tempArr)
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-    const getUsersList = async () => {
-        try {
-            const { data } = await API.get('/api/manager/list_users', header);
-            if (data) {
-                const users = data.map(user => {
-                    return {
-                        id: user?.id,
-                        text: `${user?.first_name} ${user?.last_name}`,
-                        value: user?.id,
-                    }
-                })
-                setUsersList(users);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // const getUsersList = async () => {
+    //     try {
+    //         const { data } = await API.get('/api/manager/list_users', header);
+    //         if (data) {
+    //             const users = data.map(user => {
+    //                 return {
+    //                     id: user?.id,
+    //                     text: `${user?.first_name} ${user?.last_name}`,
+    //                     value: user?.id,
+    //                 }
+    //             })
+    //             setUsersList(users);
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-    useEffect(() => {
-        getTags()
-        getUsersList();
-    }, [])
+    // useEffect(() => {
+    //     getTags()
+    //     getUsersList();
+    // }, [])
 
-    useEffect(() => {
-        getEventsList()
-    }, [])
+    // useEffect(() => {
+    //     getEventsList()
+    // }, [])
 
 
 
@@ -213,9 +213,9 @@ const index = () => {
                     <div className='w-full xl:w-[946px] mt-16 mx-auto agenda_schedular'>
                         <Scheduler
                             // remoteEvents={fetchRemote}
-                            events={eventsList}
-                            onConfirm={handleConfirm}
-                            onDelete={handleDelete}
+                            events={EVENTS}
+                            // onConfirm={handleConfirm}
+                            // onDelete={handleDelete}
                             view="week"
                             editable={true}
                             deletable={true}
@@ -237,20 +237,18 @@ const index = () => {
                                             label: "Description", required: true, min: 3, variant: "outlined",
                                         }
                                     },
-                                    {
-                                        name: "participants",
-                                        type: "select",
-                                        config: { label: "Assignee", required: true, multiple: "chips" },
-                                        // default: [1, 2],
-                                        options: usersList
-                                    },
-                                    {
-                                        name: "tag",
-                                        type: "select",
-                                        config: { label: "Tag", required: true },
-                                        // default: [1, 2],
-                                        options: tagsOptions
-                                    },
+                                    // {
+                                    //     name: "participants",
+                                    //     type: "select",
+                                    //     config: { label: "Assignee", required: true, multiple: "chips" },
+                                    //     options: usersList
+                                    // },
+                                    // {
+                                    //     name: "tag",
+                                    //     type: "select",
+                                    //     config: { label: "Tag", required: true },
+                                    //     options: tagsOptions
+                                    // },
                                 ]
                             }
                         />
